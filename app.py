48f9c20b1ce525f89893e49992f35355716734a7
suraj kumar <mahtosanoj765@gmail.com>
2026-05-28 13:40:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import telebot
 import google.generativeai as genai
 import os
 
-TOKEN = "8771761481:AAEMEeZ4Df3CCJL7-zuUN3-lRSh8rZepOI0"
-genai.configure(api_key="AIzaSyCBB3ED9lgsmShKerZTRATTW7urGkD2cXc")
+TOKEN = "8771761481:AAF5i8894_QAa5V7_dAkcUpqci8d8wEogss"
+genai.configure(api_key="AIzaSyCBB3ED9IgsmShKerZTRATTW7urGkD2cXc")
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 bot = telebot.TeleBot(TOKEN)
