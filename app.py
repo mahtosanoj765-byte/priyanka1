711057bae9243dba01c86bf95bc6730830739546
suraj kumar <mahtosanoj765@gmail.com>
2026-05-28 13:20:23 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import telebot
+import google.generativeai as genai
+import os
+
+TOKEN = "8771761481:AAEMEeZ4Df3CCJL7-zuUN3-lRSh8rZepOI0"
+genai.configure(api_key="AIzaSyCBB3ED9lgsmShKerZTRATTW7urGkD2cXc")
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    response = model.generate_content(message.text)
+    bot.reply_to(message, response.text)
+
+bot.infinity_polling()
